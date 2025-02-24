3c4a56b5265079deb035df29faac072843be8e17
RohanShelke <shelkerohan555@gmail.com>
2025-02-24 17:51:56 +0530

    String program

---
+++ RevStringApp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RevStringApp
+{
+	public static void main(String x[])
+	{
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Enter the string");
+		String str=sc.nextLine();
+		
+		 
+		System.out.println(str);
+	}
+
+}
